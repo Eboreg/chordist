8de1ffcdb08dad53d653d06a4fd2cc57045eee7e
Robert Huselius <robert@huseli.us>
2025-06-05 13:04:15 +0200

    Removed chords in wrong tuning

--- src/chordist/banjo.py
+++ src/chordist/banjo.py
@@ -18,7 +18,6 @@ class BanjoChord(InstrumentChord):
 
 BASE_CHORDS = InstrumentChordCollection(
     BanjoChord.create("A", (2, 1, 1), (2, 2, 1), (2, 3, 1), (2, 4, 1)),
-    BanjoChord.create("A", (1, 1, 1), (2, 2, 2), (2, 3, 2), (2, 4, 2)),
     BanjoChord.create("A⁷", (2, 1, 1), (2, 2, 1), (2, 3, 1), (2, 4, 1)),
     BanjoChord.create("Am", (1, 3, 1), (2, 1, 2), (2, 2, 3), (2, 4, 4)),
     BanjoChord.create("A♭", (1, 1, 1), (1, 2, 1), (1, 3, 1), (1, 4, 1)),
@@ -32,7 +31,6 @@ BASE_CHORDS = InstrumentChordCollection(
     BanjoChord.create("B♭", (3, 2, 1), (3, 3, 1), (3, 4, 1)),
     BanjoChord.create("B♭⁷", (6, 3, 1), (6, 4, 1), (7, 2, 2), (8, 1, 3)),
     BanjoChord.create("B♭m", (6, 2, 1), (6, 3, 1), (8, 1, 3), (8, 4, 4)),
-    BanjoChord.create("C", (1, 3, 1), (2, 4, 2)),
     BanjoChord.create("C", (1, 3, 1), (2, 1, 2), (2, 4, 3)),
     BanjoChord.create("C", (5, 1, 1), (5, 2, 1), (5, 3, 1), (5, 4, 1)),
     BanjoChord.create("C⁷", (8, 3, 1), (8, 4, 1), (9, 2, 2), (10, 1, 3)),
@@ -55,7 +53,6 @@ BASE_CHORDS = InstrumentChordCollection(
     BanjoChord.create("E♭", (3, 2, 1), (4, 3, 2), (5, 1, 3), (5, 4, 4)),
     BanjoChord.create("E♭⁷", (1, 1, 1), (2, 3, 2), (3, 2, 3), (5, 4, 4)),
     BanjoChord.create("E♭m", (3, 2, 1), (4, 1, 2), (4, 3, 3), (4, 4, 4)),
-    BanjoChord.create("F", (1, 3, 1), (2, 2, 2), (3, 4, 3)),
     BanjoChord.create("F", (1, 3, 1), (2, 2, 2), (3, 1, 3), (3, 4, 4)),
     BanjoChord.create("F", (10, 1, 1), (10, 2, 1), (10, 3, 1), (10, 4, 1)),
     BanjoChord.create("F⁷", (1, 3, 1), (1, 4, 1), (2, 2, 2), (3, 1, 3)),
@@ -65,7 +62,6 @@ BASE_CHORDS = InstrumentChordCollection(
     BanjoChord.create("F♯m", (2, 2, 1), (2, 3, 1), (4, 1, 3), (4, 4, 4)),
     BanjoChord.create("G"),
     BanjoChord.create("G⁷", (3, 4, 3)),
-    BanjoChord.create("G⁷", (2, 1, 1), (3, 4, 2)),
     BanjoChord.create("Gm", (3, 2, 1), (3, 3, 2)),
 )
 
